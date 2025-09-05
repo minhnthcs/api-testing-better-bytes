9d51adcb567fa1e491753450e2a805755adfbff2
mason <minh.nthcs@gmail.com>
2025-09-05 19:32:56 +0700

    lesson 2: fix comments

--- lesson-02/01-js.js
+++ lesson-02/01-js.js
@@ -1,7 +1,7 @@
 const result = "win";
-if (result == "win") {
+if (result === "win") {
   console.log("Chúc mừng! Đội của bạn đã chiến thắng!");
-} else if (result == "draw") {
+} else if (result === "draw") {
   console.log("Trận đấu hòa, thật đáng tiếc!");
 } else {
   console.log("Thua rồi, nhưng đừng bỏ cuộc!");
--- lesson-02/02-js.js
+++ lesson-02/02-js.js
@@ -1,12 +1,12 @@
 const spotifyType = "student";
 
-if (spotifyType == "free") {
+if (spotifyType === "free") {
   console.log("Bạn có thể nghe nhạc nhưng có quảng cáo.");
-} else if (spotifyType == "premium") {
+} else if (spotifyType === "premium") {
   console.log("Không quảng cáo, nghe nhạc offline!");
-} else if (spotifyType == "family") {
+} else if (spotifyType === "family") {
   console.log("Gói dùng cho nhiều thành viên!");
-} else if (spotifyType == "student") {
+} else if (spotifyType === "student") {
   console.log("Ưu đãi dành riêng cho sinh viên!");
 } else {
   console.log("Khong hop le");
